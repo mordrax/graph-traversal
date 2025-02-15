504dc9d3289a9a49016195de6b0242ea7f790873
mordrax <joseph.ni@gmail.com>
2025-02-15 12:23:14 +1100

    minimum connected edges, q2 WIP

--- src/graph_traversal/q2.clj
+++ src/graph_traversal/q2.clj
@@ -31,8 +31,8 @@
 (defn add-weight [x]
   [(num-to-keyword (inc x)) (rand-int WEIGHT)])
 
-(defn check-constraints
-  "Checks N > 0, S >= N-1, S <= N(N-1) and returns "
+(defn is-valid-graph
+  "Checks N > 0, S >= N-1, S <= N(N-1) and returns falsey if any of the constraints are not met"
   [N S]
   (let [max-connections-node (dec N)
         lower-bound max-connections-node
@@ -49,17 +49,82 @@
           :else
           true)))
 
-;; make-graph should perform the following functions, short-circuting 
-;; if any of the functions return falsey
-;; - Check the sparseness constraint
-;; - Generate a list of nodes for connectedness
-;; - Generate a list of all possible edges
-;; - Generate a list of random edges
-(defn make-graph [N S]
-  (when (check-constraints N S)
-    (->> (range N)
-         (map add-weight)
-         set)))
-
-(flatten (make-graph 5 20)) ; nil
+#_(
+  ;;  Edges are thought of as a exhausive 2D grid
+  ;;  eg, N = 5, each row starts with the first column being a node and each
+  ;;  subsequent column is a edge to another node. So a fully connected N=5 graph
+  ;;  would look like
+  ;;  0 | 1 2 3 4
+  ;;  1 | 0 2 3 4 
+  ;;  2 | 0 1 3 4 
+  ;;  3 | 0 1 2 4
+  ;;  4 | 0 1 2 3
    
+  ;;  All 20 cells (representing an edge E) is by definition
+  ;;  the complete set of possible edges for a fully connected graph of N nodes.
+
+  ;;  Therefore, for a given number (edge-index) between 1 and 20, we can represent all possible
+  ;;  edges in the graph.
+  ;;  eg, A connected graph is just edges 0 to 3
+  ;;  A random connected graph could be 0,1,2,3,4,7,16,19
+  ;;  eg, N=5, 4 neighbours, edge 19
+  ;;  row = floor(19 / 4) = 4, starting node is 4
+  ;;  col = 19 % 4 = 3, 3rd index in neighbours.
+  ;;  For node 4, neighbours are [0 1 2 3], 3rd is 3.
+  ;;  so row = 4, col = 3
+
+  ;;  This function will take a graph size and edge-index and return the absolute edge. 
+) 
+
+(defn absolute-edge-from-graph [N edge-index]
+  (when (and (> N 1) (>= edge-index 0))
+  (let [max-neighbours (dec N)
+        row (quot edge-index max-neighbours)
+        row-edge-index (rem edge-index max-neighbours)
+        neighbours (remove (fn [x] (= row x)) (range N))
+        col (get (vec neighbours) row-edge-index)
+          ;; _ (do
+          ;;    (println "N:" N)
+          ;;    (println "edge-index:" edge-index)
+          ;;    (println "max-neighbours:" max-neighbours)
+          ;;    (println "row:" row)
+          ;;    (println "row-edge-index:" row-edge-index)
+          ;;    (println "neighbours:" neighbours)
+          ;;    (println "col:" col)
+          ;; )
+        ]
+    [row col])  
+    ) 
+  )
+  
+
+;; N = 5
+;; S = 20
+;; Distinct edge E 
+;; = floor (E / N) to (E % N)
+;; Edge 1: 0 to 1
+;; Edge 5: 1
+(defn make-graph [N S]
+  (when (is-valid-graph N S)
+    (let [max-neighbours (dec N)
+          make-edge (partial absolute-edge-from-graph N)
+          minimum-connected-edges (map make-edge (range max-neighbours))
+          max-sparsity (* N max-neighbours)
+          S-after-minimum-edges (->> (count minimum-connected-edges)
+                                     (- S)
+                                     (max 0))
+          random-edges
+          (take S-after-minimum-edges
+                (shuffle (range (dec N) max-sparsity)))
+          remaining-random-edge-indexes ()]
+      minimum-connected-edges
+    ))
+)
+
+(make-graph 5 20) ; nil
+
+(take 19 (shuffle (range 0 20)))
+
+(count (take 15 (shuffle (range 4 20))))
+
+(map (partial absolute-edge-from-graph 5) (range 5))
--- test/graph_traversal/q2_test.clj
+++ test/graph_traversal/q2_test.clj
@@ -1,7 +1,10 @@
 (ns graph-traversal.q2-test
   (:require
-   [clojure.test :refer [deftest is testing]]
-   [graph-traversal.q2 :refer [make-graph]]))
+   [clojure.test :refer
+    [deftest is testing]]
+   [graph-traversal.q2 :refer
+    [make-graph
+     absolute-edge-from-graph]]))
 
 ;; define a 5 node, 20 edge fully connected graph with random weights
 (def CONNECTED {:1 [[:2 1] [:3 2] [:4 3] [:5 4]],
@@ -24,8 +27,24 @@
   (testing "generate a graph with 4 nodes and 3 edges"
     (is (not (nil? (make-graph 4 3)))))
 
-  (testing "generate a fully connected graph of 5 nodes and 20 edges"
-    (let [g (make-graph 5 20)]
-      (is (not (nil? g)))
-      (is (= 5 (count g)))
-      (is (= 40 (count (flatten (vals g))))))))
+  ;; (testing "generate a fully connected graph of 5 nodes and 20 edges"
+  ;;   (let [g (make-graph 5 20)]
+  ;;     (is (not (nil? g)))
+  ;;     (is (= 5 (count g)))
+  ;;     (is (= 40 (count (flatten (vals g)))))))
+  )
+
+(deftest test-absolute-edge-from-graph
+  (testing "generates first edge"
+    (is (= [0 1] (absolute-edge-from-graph 5 0))))
+  (testing "generates last edge"
+    (is (= [4 3] (absolute-edge-from-graph 5 19))))
+  (testing "generates invalid edge" ;20 is the first edge of next node [5 0] which does not exist, nodes 0-4
+    (is (= [5 0] (absolute-edge-from-graph 5 20))))
+  (testing "does not crash on 1 node"
+    (is (= nil (absolute-edge-from-graph 1 0))))
+  (testing "does not crash on -1 node"
+    (is (= nil (absolute-edge-from-graph -1 0))))
+
+  (testing "does not crash on -1 edge"
+    (is (= nil (absolute-edge-from-graph 5 -1)))))
