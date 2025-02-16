f98e8abcfa2233406b43fb05ab52fcfba63db7c3
mordrax <joseph.ni@gmail.com>
2025-02-16 12:26:10 +1100

    q4 setup

---
+++ src/graph_traversal/q4.clj
@@ -0,0 +1,27 @@
+;; 4. Write a suite of functions to calculate distance properties for your graph.
+;; Now that you have implemented Dijkstra's algorithm you should be able to calculate the eccentricity of any vertex in your graph, and in turn the radius and diameter of your graph.
+;; Please re-acquaint yourself with graph distance properties https://en.wikipedia.org/wiki/Distance_(graph_theory),
+
+;; The eccentricity of a vertex v is defined as the greatest distance between v and any other vertex.
+;; The radius of a graph is the minimum eccentricity of any vertex in a graph.
+;; The diameter of a graph is the maximum eccentricity of any vertex in a graph.
+;; I should be able to write something like this:
+
+;; #!clojure
+;; (def random-graph (make-graph 10 10))
+;; (eccentricity random-graph (first (keys random-graph))) ; => number expressing eccentricity for `first` vertex in random-graph
+;; (radius random-graph) ; => minimal eccentricity
+;; (diameter random-graph) ; => maximal eccentricity
+
+(ns graph-traversal.q4
+  (:require
+   [clojure.pprint :as pprint]))
+
+
+(defn eccentricity [graph node])
+(defn radius [graph])
+(defn diameter [graph])
+
+(eccentricity {} :1)
+(radius {})
+(diameter {})
---
+++ test/graph_traversal/q4_test.clj
@@ -0,0 +1,6 @@
+(ns graph-traversal.q4-test
+  (:require
+   [clojure.test :refer
+    [deftest is testing]]
+   [graph-traversal.q4 :refer
+    [eccentricity radius diameter]]))
